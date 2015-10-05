7d33352a904eaf391f48cacab1d6bad71faa85d8
Antonio Kless <antoniok.spb@gmail.com>
2015-10-05 10:21:00 +0300

    remove --force-yes options
    Remove unneeded --force-yes options from apt-get calls

--- os-stuff/Ubuntu/after-install.sh
+++ os-stuff/Ubuntu/after-install.sh
@@ -5,7 +5,7 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-apt-get -y --force-yes update
+apt-get -y update
 apt-get -y --force-yes upgrade
 
 dpkg --purge \
@@ -24,7 +24,7 @@ dpkg --purge \
     ttf-lao\
     ttf-punjabi-fonts
 
-apt-get -y --force-yes install \
+apt-get -y install \
     acpi\
     tree\
     htop\
@@ -52,23 +52,23 @@ apt-get -y --force-yes install \
     libssl-doc
 
 # installing Windows fonts from repo:
-#apt-get -y --force-yes install msttcorefonts && fc-cache -fv
+#apt-get -y install msttcorefonts && fc-cache -fv
 
 # installing Dropbox from repo:
 #lsb_release -c | awk '{print "deb http://linux.dropbox.com/ubuntu " $2 " main"}' >> /etc/apt/sources.list
 #apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
 #apt-get update
-#apt-get -y --force-yes install python-gpgme dropbox
+#apt-get -y install python-gpgme dropbox
 
 # for nvidia videocards:
-#apt-get -y --force-yes install nvidia-current-updates nvidia-settings-updates
+#apt-get -y install nvidia-current-updates nvidia-settings-updates
 
 # for touch-screens:
-#apt-get -y --force-yes install xinput-calibrator
+#apt-get -y install xinput-calibrator
 
 # for kvm virtualization:
-#apt-get -y --force-yes install qemu qemuctl
+#apt-get -y install qemu qemuctl
 
-apt-get -y --force-yes autoremove
+apt-get -y autoremove
 
 mkdir -p ~/.compose-cache
